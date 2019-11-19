4e774a5877ddacd8fd2bc295497b6bc577de8d8c
paolomandica <paolomandica.p@gmail.com>
2019-11-19 15:04:19 +0100

    IMPLEMENTED alphabetical_couting_sort function.

--- sorting_lib.py
+++ sorting_lib.py
@@ -1,4 +1,5 @@
-def counting_sort(A, k):
+def counting_sort(A):
+    k = max(A)
     n = len(A)
     B = [0 for i in range(n)]
     C = [0 for i in range(k+1)]
@@ -20,11 +21,19 @@ def counting_sort(A, k):
     return B
     
 
+def alphabetical_couting_sort(lst):
+    A = [(ord(c)-97) for c in lst]
+    B = counting_sort(A)
+    return [chr(x+97) for x in B]
+
+
 if __name__ == "__main__":
     array = [4,7,2,4,2,8,5,6,1,2,5]
-    # array = [2,4,1,0,1]
-    k = max(array)
-    ordered_array = counting_sort(array, k)
-    print(*array)
-    print(*ordered_array)
+    # ordered_array = counting_sort(array)
+    # print(*array)
+    # print(*ordered_array)
+    lst = ['c','z','e','i','d','g','j','q','r','a']
+    ordered_alpha = alphabetical_couting_sort(lst)
+    print(*lst)
+    print(*ordered_alpha)
     
