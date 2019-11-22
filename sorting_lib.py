4f50d775bc8fe724ebf6346d5cec1e1c6d7aabbf
paolomandica <paolomandica.p@gmail.com>
2019-11-22 08:28:57 +0100

    UPDATED and resolved some bugs. Started to work on complexity.

--- sorting_lib.py
+++ sorting_lib.py
@@ -1,7 +1,16 @@
-n_chars = 64
+# Discussions about time complexity are on the main.ipynb file.
+
+diff = ord('A') - 1
 
 
 def counting_sort(A):
+    """
+    This function takes in input a list of integers and return a new ordered list, based on the first one.
+    :param A: the list to order
+    :type A: list of int
+    :return: ordered list
+    :rtype: list of int
+    """
     k = max(A)
     n = len(A)
     B = [0 for i in range(n)]
@@ -25,9 +34,9 @@ def counting_sort(A):
 
 
 def char_couting_sort(lst):
-    A = [(ord(c)-n_chars) for c in lst]
+    A = [(ord(c)-diff) for c in lst]
     B = counting_sort(A)
-    return [chr(x+n_chars) for x in B]
+    return [chr(x+diff) for x in B]
 
 
 def my_groupby(lst, i):
@@ -40,29 +49,41 @@ def my_groupby(lst, i):
 def list_counting_sort(A, j):
     k = ord('z') - ord('A')
     n = len(A)
-    B = [0 for i in range(n)]
     C = [0 for i in range(k+1)]
+    to_return = []
 
     for i in range(n):
-        if A[i] == ' ':
+        if len(A[i]) <= j:
+            to_return.append(A[i])
+            continue
+
+        if A[i][j] == ' ':
             index = 0
         else:
-            index = ord(A[i][j])-n_chars
+            index = ord(A[i][j])-diff
         C[index] += 1
         # each element C[i] is equal to the number of i in the array A
+
+    for word in to_return:
+        A.remove(word)
 
     for i in range(1, k):
         C[i] += C[i-1]
         # each C[i] is now equal to the number of elements in A equal or less than i
 
+    n = len(A)
+    B = [0 for i in range(n)]
     for i in range(n-1, -1, -1):
-        char = ord(A[i][j])-n_chars
+        char = ord(A[i][j])-diff
         index = C[char]-1
         B[index] = A[i]  # insert A[i] in the right position in B, given by C[i]-1
         C[char] += -1  # decrease by one the number of elements equal or less than A[i]
 
     # return [list(g) for k, g in groupby(B, key=lambda x: x[j])]
-    return my_groupby(B, j)
+    grouped_list = my_groupby(B, j)
+    if len(to_return) > 0:
+        grouped_list.insert(0, to_return)
+    return grouped_list
 
 
 def rec_alphabetical_sort(lst, j):
@@ -92,7 +113,9 @@ if __name__ == "__main__":
     # print(*lst)
     # print(*ordered_alpha)
 
-    string_lst2 = ['paolo', 'lorenzo', 'giovanni', 'emanu ele', 'emanu ela', 'paolo']
-    ordered_strings = rec_alphabetical_sort(string_lst2, 0)
-    print(*string_lst2)
+    l1 = ['paolo', 'lorenzo', 'emanu ele', 'emanu ela', 'paolo', 'paol']
+    l = ['paolo', 'paol']
+    es1 = ['As', 'Aster', 'Astrolabe', 'Astronomy', 'Astrophysics', 'At', 'Ataman', 'Attack', 'Baa']
+    ordered_strings = rec_alphabetical_sort(es1, 0)
+    print(*es1)
     print(*ordered_strings)
